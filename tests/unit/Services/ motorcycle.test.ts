80ef03295d5328a5ce95f22a80a5b5ffe0f6e3ec
Joao_Vitor <2013joaovitor10@Gmail.com>
2023-04-27 18:28:49 -0300

    requisito de testes da service chegando a 60% no código atual

--- tests/unit/Services/ motorcycle.test.ts
+++ tests/unit/Services/ motorcycle.test.ts
@@ -1,72 +1,97 @@
-// import { expect } from 'chai';
-// import sinon from 'sinon';
-// import { Model } from 'mongoose';
-// import Car from '../../../src/Domains/Car';
-// import CarsService from '../../../src/Services/CarsService';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import MotorcyclesService from '../../../src/Services/MotorcycleService';
+import Motorcycle from '../../../src/Domains/Motorcycle';
 
-// const inputArray = [
-//   {
-//     id: '644989181b48cdab59bc6627',
-//     model: 'Marea',
-//     year: 2002,
-//     color: 'Black',
-//     status: false,
-//     buyValue: 15.99,
-//     doorsQty: 4,
-//     seatsQty: 5,
-//   },
-//   {
-//     id: '644989191b48cdab59bc6629',
-//     model: 'Marea',
-//     year: 2002,
-//     color: 'Black',
-//     status: false,
-//     buyValue: 15.99,
-//     doorsQty: 4,
-//     seatsQty: 5,
-//   },
-// ];
+const inputArray = [
+  {
+    id: '644ae5a138761dc44bcacf45',
+    model: 'Honda Cb 600f Hornet',
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30,
+    engineCapacity: 600,
+    category: 'Street',
+  },
+  {
+    id: '644ae5b238761dc44bcacf4a',
+    model: 'Honda Cb 600f Hornet',
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30,
+    engineCapacity: 600,
+    category: 'Street',
+  },
+];
 
-// describe('it should test all carsService functions', function () {
-//   it('it should list all cars', async function () {
-//     // Arrange
-//     const carOutput = inputArray.map((item) => new Car(
-//       {
-//         model: item.model,
-//         year: item.year,
-//         color: item.color, 
-//         status: item.status, 
-//         buyValue: item.buyValue, 
-//         doorsQty: item.doorsQty, 
-//         seatsQty: item.seatsQty,
-//       },
-//     ));
+describe('it should test all motorcyclesService functions', function () {
+  it('it should list all motorcycles', async function () {
+    // Arrange
+    const motorcycleOutPut = inputArray.map((item) => new Motorcycle(
+      {
+        model: item.model,
+        year: item.year,
+        color: item.color, 
+        status: item.status, 
+        buyValue: item.buyValue, 
+        category: item.category,
+        engineCapacity: item.engineCapacity,
+      },
+    ));
     
-//     sinon.stub(Model, 'find').resolves(carOutput);
+    sinon.stub(Model, 'find').resolves(motorcycleOutPut);
 
-//     // Act
-//     const service = new CarsService();
-//     const result = await service.find();
+    // Act
+    const service = new MotorcyclesService();
+    const result = await service.find();
 
-//     // Assert
-//     expect(result).to.be.deep.equal(carOutput);
+    // Assert
+    expect(result).to.be.deep.equal(motorcycleOutPut);
 
-//     sinon.restore();
-//   });
+    sinon.restore();
+  });
 
-//   it('should list one car with a specific id', async function () {
-//     // Arrange
-//     const carOutput = new Car(inputArray[0]);
+  it('should list one motorcycle with a specific id', async function () {
+    // Arrange
+    const motorcycleOutput = new Motorcycle(inputArray[0]);
     
-//     sinon.stub(Model, 'findById').resolves(carOutput);
+    sinon.stub(Model, 'findById').resolves(motorcycleOutput);
 
-//     // Act
-//     const service = new CarsService();
-//     const result = await service.findById('644989181b48cdab59bc6627');
+    // Act
+    const service = new MotorcyclesService();
+    const result = await service.findById('644ae5a138761dc44bcacf45');
 
-//     // Assert
-//     expect(result).to.be.deep.equal({ data: carOutput, status: 200 });
+    // Assert
+    expect(result).to.be.deep.equal({ data: motorcycleOutput, status: 200 });
 
-//     sinon.restore();
-//   });
-// });
+    sinon.restore();
+  });
+
+  it('should create a motorcycle', async function () {
+    // Arrange
+    const motorcycleOutput = new Motorcycle(inputArray[0]);
+    
+    sinon.stub(Model, 'create').resolves(motorcycleOutput);
+
+    // Act
+    const service = new MotorcyclesService();
+    const result = await service.create(inputArray[0]);
+
+    // Assert
+    expect(result).to.be.deep.equal({
+      buyValue: 30,
+      category: 'Street',
+      color: 'Yellow',
+      engineCapacity: 600,
+      id: '644ae5a138761dc44bcacf45',
+      model: inputArray[0].model,
+      status: true,
+      year: 2005,
+    });
+
+    sinon.restore();
+  });
+});
